6dc8eeb8f55fe6dd22b9ee31e03efc388e35e3bc
Jared Grimes <jaredlancegrimes@gmail.com>
2025-12-29 19:30:58 -0600

    edited the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white dark:bg-black sm:items-start'>
+      <main className='flex p-8 min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white dark:bg-black sm:items-start'>
+        <div>
+          <p>woop. for all your stuff.</p>
+          <p>coming soon.foo</p>
+        </div>
         <h1 className='text-2xl font-bold'>woop.foo</h1>
       </main>
     </div>
