0c8605f5014b0181c8966c58b0661bd3cb85ac52
Jared Grimes <jaredlancegrimes@gmail.com>
2025-12-29 19:39:22 -0600

    removed the white thing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <main className='flex p-8 min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white dark:bg-black sm:items-start'>
+      <main className='flex p-8 min-h-screen w-full max-w-3xl flex-col items-center justify-between sm:items-start'>
         <div>
           <p>woop. for all your stuff.</p>
           <p>coming soon.foo</p>
