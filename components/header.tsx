a3c04ab211919e09a0ae96d2f99c6527520e9326
Jared Grimes <jaredgrimes@microsoft.com>
2026-01-02 14:28:40 -0600

    removed woop anim

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import { FlipWords } from './ui/flip-words';
-
 export function Header({ ip }: { ip: string }) {
   return (
     <div>
       <h1 className="text-2xl font-bold flex items-center">
-        <FlipWords words={['woop']} once className="px-0" />
+        woop
       </h1>
       <h2 className="text-xl font-semibold text-muted-foreground font-mono">
         {ip}
